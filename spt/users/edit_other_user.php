3eda231c6bfd02f8b80fbc7a4453df077202d227
1 <1@sptoolkit.com>
2011-11-19 12:30:14 -0600

    fixed problem where editing another user's password would break it due to some old md5 hashing code

--- spt/users/edit_other_user.php
+++ spt/users/edit_other_user.php
@@ -185,7 +185,7 @@ if(!empty($_POST['password']))
 			}
 		
 		//pass temp password to new variable that has been salted and hashed
-		$p = md5($_SESSION['salt'].$temp_p.$_SESSION['salt']);
+		$p = sha1($_SESSION['salt'].$temp_p.$_SESSION['salt']);
 	}
 
 //validate and set the admin and disabled parameters
