1ef3480f5c48991ecf1657f9f5053fed0cfa0612
1 <1@sptoolkit.com>
2012-02-25 18:07:29 -0600

    Start Campaign script does not send emails anymore
    The start campaign script now just updates the database with all
    relevant information to get the campaign started…it doesn't actually
    send the emails.  The emails will be sent by a send_emails.php script
    that will be coming soon.  This commit now breaks all email sending. :)

--- spt_0.60/spt/campaigns/start_campaign.php
+++ spt_0.60/spt/campaigns/start_campaign.php
@@ -2,7 +2,7 @@
 
 /**
  * file:    start_campaign.php
- * version: 17.0
+ * version: 18.0
  * package: Simple Phishing Toolkit (spt)
  * component:   Campaign management
  * copyright:   Copyright (C) 2011 The SPT Project. All rights reserved.
@@ -22,7 +22,6 @@
  * You should have received a copy of the GNU General Public License
  * along with spt.  If not, see <http://www.gnu.org/licenses/>.
  * */
-
 // verify session is authenticated and not hijacked
 $includeContent = "../includes/is_authenticated.php";
 if ( file_exists ( $includeContent ) ) {
@@ -40,50 +39,61 @@ if ( file_exists ( $includeContent ) ) {
 }
 
 //ensure the campaign name is set
-if ( ! isset ( $_POST['campaign_name'] ) ) {
-    $_SESSION['alert_message'] = "you must give the campaign a name";
+if ( ! isset ( $_POST[ 'campaign_name' ] ) ) {
+    $_SESSION[ 'alert_message' ] = "you must give the campaign a name";
     header ( 'location:./#alert' );
     exit;
 }
 
 //ensure a target group was selected
-if ( ! isset ( $_POST['target_groups'] ) ) {
-    $_SESSION['alert_message'] = "please select at least one target group";
+if ( ! isset ( $_POST[ 'target_groups' ] ) ) {
+    $_SESSION[ 'alert_message' ] = "please select at least one target group";
     header ( 'location:./#alert' );
     exit;
 }
 
 //ensure a template is selected
-if ( ! isset ( $_POST['template_id'] ) ) {
-    $_SESSION['alert_message'] = "please select a template";
+if ( ! isset ( $_POST[ 'template_id' ] ) ) {
+    $_SESSION[ 'alert_message' ] = "please select a template";
     header ( 'location:./#alert' );
     exit;
 }
 
 //recieve the entered values and put into variables
-$campaign_name = filter_var ( $_POST['campaign_name'], FILTER_SANITIZE_STRING );
-$spt_path = $_POST['spt_path'];
-$target_groups = $_POST['target_groups'];
-$template_id = filter_var ( $_POST['template_id'], FILTER_SANITIZE_NUMBER_INT );
-$education_id = filter_var ( $_POST['education_id'], FILTER_SANITIZE_NUMBER_INT );
+$campaign_name = filter_var ( $_POST[ 'campaign_name' ], FILTER_SANITIZE_STRING );
+$spt_path = $_POST[ 'spt_path' ];
+$target_groups = $_POST[ 'target_groups' ];
+$template_id = filter_var ( $_POST[ 'template_id' ], FILTER_SANITIZE_NUMBER_INT );
+$education_id = filter_var ( $_POST[ 'education_id' ], FILTER_SANITIZE_NUMBER_INT );
+$message_delay = filter_var ( $_POST[ 'message_delay' ], FILTER_SANITIZE_NUMBER_INT );
 $date_sent = date ( "F j, Y, g:i a" );
-if ( isset ( $_POST['education_timing'] ) ) {
-    $education_timing = filter_var ( $_POST['education_timing'], FILTER_SANITIZE_NUMBER_INT );
+if ( isset ( $_POST[ 'education_timing' ] ) ) {
+    $education_timing = filter_var ( $_POST[ 'education_timing' ], FILTER_SANITIZE_NUMBER_INT );
 }
 
 //connect to database
 include "../spt_config/mysql_config.php";
 
+//ensure there is not already an active campaign
+$r = mysql_query ( "SELECT status FROM campaigns" );
+while ( $ra = mysql_fetch_assoc ( $ra ) ) {
+    if ( $ra[ 'status' ] == 1 ) {
+        $_SESSION[ 'alert_message' ] = "there is already an active campaign, cancel it or let it finish before starting a new one";
+        header ( 'location:./#alert' );
+        exit;
+    }
+}
+
 //take each value in the array and validate that it is a valid group name
 foreach ( $target_groups as $group ) {
     $r = mysql_query ( "SELECT DISTINCT group_name FROM targets" ) or die ( '<!DOCTYPE HTML><html><body><div id="die_error">There is a problem with the database...please try again later</div></body></html>' );
     while ( $ra = mysql_fetch_assoc ( $r ) ) {
-        if ( $group == $ra['group_name'] ) {
+        if ( $group == $ra[ 'group_name' ] ) {
             $match = 1;
         }
     }
     if ( ! isset ( $match ) ) {
-        $_SESSION['alert_message'] = "invalid group";
+        $_SESSION[ 'alert_message' ] = "invalid group";
         header ( 'location:./#alert' );
         exit;
     }
@@ -92,12 +102,12 @@ foreach ( $target_groups as $group ) {
 //validate the template exists
 $r = mysql_query ( "SELECT id FROM templates" ) or die ( '<!DOCTYPE HTML><html><body><div id="die_error">There is a problem with the database...please try again later</div></body></html>' );
 while ( $ra = mysql_fetch_assoc ( $r ) ) {
-    if ( $template_id == $ra['id'] ) {
+    if ( $template_id == $ra[ 'id' ] ) {
         $match0 = 1;
     }
 }
 if ( ! isset ( $match0 ) ) {
-    $_SESSION['alert_message'] = "please select a valid template";
+    $_SESSION[ 'alert_message' ] = "please select a valid template";
     header ( 'location:./#alert' );
     exit;
 }
@@ -105,12 +115,12 @@ if ( ! isset ( $match0 ) ) {
 //validate the education package exists
 $r = mysql_query ( 'SELECT id FROM education' ) or die ( '<!DOCTYPE HTML><html><body><div id="die_error">There is a problem with the database...please try again later</div></body></html>' );
 while ( $ra = mysql_fetch_assoc ( $r ) ) {
-    if ( $education_id == $ra['id'] OR $education_id == 0 ) {
+    if ( $education_id == $ra[ 'id' ] OR $education_id == 0 ) {
         $match1 = 1;
     }
 }
 if ( ! isset ( $match1 ) ) {
-    $_SESSION['alert_message'] = "please select a valid education package";
+    $_SESSION[ 'alert_message' ] = "please select a valid education package";
     header ( 'location:./#alert' );
     exit;
 }
@@ -125,18 +135,43 @@ if ( isset ( $education_timing ) ) {
     $match2 = 1;
 }
 if ( $match2 != 1 ) {
-    $_SESSION['alert_message'] = "please select a valid education timing option";
+    $_SESSION[ 'alert_message' ] = "please select a valid education timing option";
     header ( 'location:./#alert' );
     exit;
 }
 
+//validate the message delay
+if ( isset ( $message_delay ) ) {
+    //ensure the message delay is greater than 100 ms
+    if ( $message_delay < 100 ) {
+        $_SESSION[ 'alert_message' ] = "the message delay factor must be greater than 100ms";
+        header ( 'location:./#alert' );
+        exit;
+    }
+    //ensure the message delay is in incrmeents of 100
+    if ( substr ( $message_delay, -2 ) != "00" ) {
+        $_SESSION[ 'alert_message' ] = "the message delay factor should be in increments of 100ms";
+        header ( 'location:./#alert' );
+        exit;
+    }
+    //ensure the message delay is not greater than 1 minute
+    if ( $message_delay > 60000 ) {
+        $_SESSION[ 'alert_message' ] = "the message delay factor cannot be more than 1 minute";
+        header ( 'location:./#alert' );
+        exit;
+    }
+} else {
+    //if for some reason the message delay is not set, give it the default value of 1 second
+    $message_delay = 1000;
+}
+
 //create the campaign
-mysql_query ( "INSERT INTO campaigns (campaign_name, template_id, domain_name, education_id, education_timing, date_sent) VALUES ('$campaign_name', '$template_id', '$spt_path', '$education_id', '$education_timing', '$date_sent')" ) or die ( '<!DOCTYPE HTML><html><body><div id="die_error">There is a problem with the database...please try again later</div></body></html>' );
+mysql_query ( "INSERT INTO campaigns (campaign_name, template_id, domain_name, education_id, education_timing, date_sent, message_delay, status) VALUES ('$campaign_name', '$template_id', '$spt_path', '$education_id', '$education_timing', '$date_sent', '$message_delay', 1)" ) or die ( '<!DOCTYPE HTML><html><body><div id="die_error">There is a problem with the database...please try again later</div></body></html>' );
 
 //get the id of this campaign
 $r = mysql_query ( "SELECT MAX(id) as campaign_id FROM campaigns" ) or die ( '<!DOCTYPE HTML><html><body><div id="die_error">There is a problem with the database...please try again later</div></body></html>' );
 while ( $ra = mysql_fetch_assoc ( $r ) ) {
-    $campaign_id = $ra['campaign_id'];
+    $campaign_id = $ra[ 'campaign_id' ];
 }
 
 //link the campaign id and group name while retrieving all applicable targets
@@ -147,7 +182,7 @@ foreach ( $target_groups as $group ) {
     //retrieve all targets from group
     $r = mysql_query ( "SELECT id FROM targets WHERE group_name = '$group'" ) or die ( '<!DOCTYPE HTML><html><body><div id="die_error">There is a problem with the database...please try again later</div></body></html>' );
     while ( $ra = mysql_fetch_assoc ( $r ) ) {
-        $target_id = $ra['id'];
+        $target_id = $ra[ 'id' ];
 
         //generate a random key
         $random_number = mt_rand ( 1000000000, 9999999999 );
@@ -158,40 +193,6 @@ foreach ( $target_groups as $group ) {
     }
 }
 
-//get all the necessary email addresses
-$r = mysql_query ( "SELECT targets.fname AS fname, targets.lname AS lname, targets.email as email, targets.id as id, campaigns_responses.response_id as response_id FROM campaigns_responses JOIN targets ON targets.id = campaigns_responses.target_id WHERE campaigns_responses.campaign_id = '$campaign_id'" ) or die ( '<!DOCTYPE HTML><html><body><div id="die_error">There is a problem with the database...please try again later</div></body></html>' );
-
-//using the built-in php mail function which depends on the php.ini file and the servers mail settings
-while ( $ra = mysql_fetch_assoc ( $r ) ) {
-    //set the current email address
-    $current_target_email_address = $ra['email'];
-    $current_target_id = $ra['id'];
-    $current_response_id = $ra['response_id'];
-    $fname = $ra['fname'];
-    $lname = $ra['lname'];
-
-    //formulate link
-    $link = "http://" . $spt_path . "/campaigns/response.php?r=" . $current_response_id;
-
-    //pull in all the email variables from the specified template
-    include "../templates/" . $template_id . "/email.php";
-
-    //find and replace variables
-    $message = preg_replace ( "#@fname#", $fname, $message );
-    $message = preg_replace ( "#@lname#", $lname, $message );
-    $message = html_entity_decode ( $message );
-    $subject = preg_replace ( "#@fname#", $fname, $subject );
-    $subject = preg_replace ( "#@lname#", $lname, $subject );
-
-    //send the email and force the envelope sender if the email template specifies
-    if ( isset ( $f_sender ) ) {
-        mail ( $current_target_email_address, $subject, $message, $headers, $f_sender );
-    } else {
-        mail ( $current_target_email_address, $subject, $message, $headers );
-    }
-}
-
-//send them back after your finished sending emails
-$_SESSION['alert_message'] = "emails have been sent...sit back and wait for the responses :)";
-header ( 'location:./#alert' );
+//send to the response page for their campaign
+header ( 'location:./?c='.$campaign_id.'#responses' );
 ?>
