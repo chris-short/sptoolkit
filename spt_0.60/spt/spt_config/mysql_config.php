ce980cbc98f2f3fb4e3e7bf2deac5b309d1961cb
42spt <42@no-spam.sptoolkit.com>
2012-03-04 08:33:57 -0600

    Code cleanup in spt_config.php.

--- spt_0.60/spt/spt_config/mysql_config.php
+++ spt_0.60/spt/spt_config/mysql_config.php
@@ -1,11 +1,12 @@
 <?php
+
 /**
- * file:		mysql_config.php
- * version:		1.0
- * package:		Simple Phishing Toolkit (spt)
+ * file:    mysql_config.php
+ * version: 2.0
+ * package: Simple Phishing Toolkit (spt)
  * component:	Core files
  * copyright:	Copyright (C) 2011 The SPT Project. All rights reserved.
- * license:		GNU/GPL, see license.htm.
+ * license: GNU/GPL, see license.htm.
  * 
  * This file is part of the Simple Phishing Toolkit (spt).
  * 
@@ -20,21 +21,21 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with spt.  If not, see <http://www.gnu.org/licenses/>.
-**/
+ * */
 
-	//this file is used throughout spt to connect to the database
-	//WARNING: if you change anything below it might break connectivity to the database
-	
-	//set the database variables
-	$mysql_host='replace_me';
-	$mysql_user='replace_me';
-	$mysql_password='replace_me';
-	$mysql_db_name='replace_me';
-	
-	//connect to the database
-	mysql_connect($mysql_host, $mysql_user, $mysql_password)or die("Cannot connect to the database"); 
-	mysql_select_db($mysql_db_name)or die("Cannot select the database");
-	
-	//create a session for the database name
-	$_SESSION['spt_db_name'] = $mysql_db_name;
+//this file is used throughout spt to connect to the database
+//WARNING: if you change anything below it might break connectivity to the database
+
+//set the database variables
+$mysql_host = 'replace_me';
+$mysql_user = 'replace_me';
+$mysql_password = 'replace_me';
+$mysql_db_name = 'replace_me';
+
+//connect to the database
+mysql_connect ( $mysql_host, $mysql_user, $mysql_password ) or die ( "Cannot connect to the database" );
+mysql_select_db ( $mysql_db_name ) or die ( "Cannot select the database" );
+
+//create a session for the database name
+$_SESSION['spt_db_name'] = $mysql_db_name;
 ?>
