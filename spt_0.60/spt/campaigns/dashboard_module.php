a9e56dfc71dddd4e3a3c4848e0f1a386ee2175f9
1 <1@sptoolkit.com>
2012-03-04 16:12:39 -0600

    New Dashboard Prep
    I've laid the groundwork for the new dashboard.  We still need to
    decide on what charts we want to display and what charting "library" we
    want to use.
    
    http://webdesigneraid.com/html5-canvas-graphing-solutions-every-web-deve
    lopers-must-know/
    
    http://www.elated.com/articles/snazzy-animated-pie-chart-html5-jquery/
    
    http://coding.smashingmagazine.com/2011/09/23/create-an-animated-bar-gra
    ph-with-html-css-and-jquery/

--- spt_0.60/spt/campaigns/dashboard_module.php
+++ spt_0.60/spt/campaigns/dashboard_module.php
@@ -1,6 +1,6 @@
 <!--
  * file:    dashboard_module.php
- * version: 3.0
+ * version: 4.0
  * package: Simple Phishing Toolkit (spt)
  * component:   Campaign management
  * copyright:   Copyright (C) 2011 The SPT Project. All rights reserved.
@@ -21,42 +21,64 @@
  * along with spt.  If not, see <http://www.gnu.org/licenses/>.
 -->
 
-<h1><a href="../campaigns">Campaigns</a></h1>
-<table>
-    <tr>
-        <td>Number of Campaigns</td>
-        <?php
-        //determine how many campaigns there are
-        $r = mysql_query ( "SELECT * FROM campaigns" ) or die ( '<div id="die_error">There is a problem with the database...please try again later</div>' );
-        echo "<td>" . mysql_num_rows ( $r ) . "</td>";
-        ?>
-    </tr>
-    <tr>
-        <td>Number of Phished Targets</td>
-        <?php
-        //determine how many targets have been phished
-        $r = mysql_query ( "SELECT * FROM campaigns_responses" ) or die ( '<div id="die_error">There is a problem with the database...please try again later</div>' );
-        echo "<td>" . mysql_num_rows ( $r ) . "</td>";
-        ?>
-    </tr>
-    <tr>
-        <td>Total Links Clicked</td>
-        <?php
-        //determine how many targets have clicked the link
-        $r = mysql_query ( "SELECT SUM(link) AS total_count FROM campaigns_responses" ) or die ( '<div id="die_error">There is a problem with the database...please try again later</div>' );
-        while ( $ra = mysql_fetch_assoc ( $r ) ) {
-            echo "<td>" . $ra['total_count'] . "</td>";
-        }
-        ?>
-    </tr>
-    <tr>
-        <td>Total Posts</td>
-        <?php
-        //determine how many targets have posted data
-        $r = mysql_query ( "SELECT COUNT(post) AS total_count FROM campaigns_responses WHERE post != \"\"" ) or die ( '<div id="die_error">There is a problem with the database...please try again later</div>' );
-        while ( $ra = mysql_fetch_assoc ( $r ) ) {
-            echo "<td>" . $ra['total_count'] . "</td>";
-        }
-        ?>
-    </tr>
-</table>
+<article class="tabs">
+    <section id="risk_pie">
+        <a href="#risk_pie"><span>Risk Pie</span></a>
+        <div>Pie Chart of overall status of links/posts/etc</div>
+    </section>
+    <section id="bad_targets">
+        <a href="#bad_targets"><span>Bad Targets</span></a>
+        <div>bar chart of targets who fall for phishing the most</div>
+    </section>
+    <section id="email_stats">
+        <a href="#email_stats"><span>Email Stats</span></a>
+        <div>pie chart of email status unknown/failed/success</div>
+    </section>
+    <section id="browser_stats">
+        <a href="#browser_stats"><span>Browser Stats</span></a>
+        <div>pie chart of browsers</div>
+    </section>
+    <section id="simple_stats">
+        <a href="#simple_stats"><span>Simple Stats</span></a>
+        <div>    
+            <table>
+                <tr>
+                    <td>Number of Campaigns</td>
+                    <?php
+                    //determine how many campaigns there are
+                    $r = mysql_query ( "SELECT * FROM campaigns" ) or die ( '<div id="die_error">There is a problem with the database...please try again later</div>' );
+                    echo "<td>" . mysql_num_rows ( $r ) . "</td>";
+                    ?>
+                </tr>
+                <tr>
+                    <td>Number of Phished Targets</td>
+                    <?php
+                    //determine how many targets have been phished
+                    $r = mysql_query ( "SELECT * FROM campaigns_responses" ) or die ( '<div id="die_error">There is a problem with the database...please try again later</div>' );
+                    echo "<td>" . mysql_num_rows ( $r ) . "</td>";
+                    ?>
+                </tr>
+                <tr>
+                    <td>Total Links Clicked</td>
+                    <?php
+                    //determine how many targets have clicked the link
+                    $r = mysql_query ( "SELECT SUM(link) AS total_count FROM campaigns_responses" ) or die ( '<div id="die_error">There is a problem with the database...please try again later</div>' );
+                    while ( $ra = mysql_fetch_assoc ( $r ) ) {
+                        echo "<td>" . $ra['total_count'] . "</td>";
+                    }
+                    ?>
+                </tr>
+                <tr>
+                    <td>Total Posts</td>
+                    <?php
+                    //determine how many targets have posted data
+                    $r = mysql_query ( "SELECT COUNT(post) AS total_count FROM campaigns_responses WHERE post != \"\"" ) or die ( '<div id="die_error">There is a problem with the database...please try again later</div>' );
+                    while ( $ra = mysql_fetch_assoc ( $r ) ) {
+                        echo "<td>" . $ra['total_count'] . "</td>";
+                    }
+                    ?>
+                </tr>
+            </table>
+        </div>
+    </section>
+</article>
