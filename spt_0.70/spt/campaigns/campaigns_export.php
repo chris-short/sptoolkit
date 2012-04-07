878057f6e3d45b26e19502274ce6dd1d78fa15ef
1 <1@sptoolkit.com>
2012-04-06 21:59:31 -0500

    Target Metrics part of Campaign Export now
    Now when you export campaign results you'll get your custom target
    metrics as well for further, pertinent analysis

--- spt_0.70/spt/campaigns/campaigns_export.php
+++ spt_0.70/spt/campaigns/campaigns_export.php
@@ -2,7 +2,7 @@
 
 /**
  * file:    campaigns_export.php
- * version: 5.0
+ * version: 6.0
  * package: Simple Phishing Toolkit (spt)
  * component:   Campaign management
  * copyright:   Copyright (C) 2011 The SPT Project. All rights reserved.
@@ -22,7 +22,6 @@
  * You should have received a copy of the GNU General Public License
  * along with spt.  If not, see <http://www.gnu.org/licenses/>.
  * */
-
 // verify session is authenticated and not hijacked
 $includeContent = "../includes/is_authenticated.php";
 if ( file_exists ( $includeContent ) ) {
@@ -42,14 +41,56 @@ if ( file_exists ( $includeContent ) ) {
 //connect to database
 include "../spt_config/mysql_config.php";
 
-//set header
-$output = "Campaign,Template,Education,EducationTiming,Date,Year,Time,FirstName,LastName,TargetEmail,LinkTime,Post,IP,Browser,OS\n";
+//get all the custom metrics from the targets table and into an array
+$metrics = mysql_query ( "SHOW COLUMNS FROM targets" );
 
+//formulate sql query
+$sql = "SELECT campaigns.campaign_name AS campaign_name, templates.name AS template_name, education.name AS education_name, campaigns.education_timing AS education_timing, campaigns.date_sent AS date_sent, campaigns_responses.post AS post, targets.email AS email, targets.fname AS fname, targets.lname AS lname, campaigns_responses.ip AS ip, campaigns_responses.browser AS browser, campaigns_responses.browser_version AS browser_version, campaigns_responses.os AS os, campaigns_responses.link_time AS link_time, targets.group_name AS group_name";
+while ( $metrics_results = mysql_fetch_array ( $metrics ) ) {
+    if ( $metrics_results['Field'] != "id" && $metrics_results['Field'] != "email" && $metrics_results['Field'] != "fname" && $metrics_results['Field'] != "lname" && $metrics_results['Field'] != "group_name" ) {
+        $sql .= ", targets." . $metrics_results['Field'] . " AS " . $metrics_results['Field'];
+    }
+}
+$sql .= " FROM campaigns_responses JOIN targets ON campaigns_responses.target_id=targets.id JOIN campaigns ON campaigns.id = campaigns_responses.campaign_id LEFT JOIN education ON campaigns.education_id = education.id JOIN templates ON templates.id = campaigns.template_id";
 //get data
-$r = mysql_query ( "SELECT campaigns.campaign_name AS campaign_name, templates.name AS template_name, education.name AS education_name, campaigns.education_timing AS education_timing, campaigns.date_sent AS date_sent, campaigns_responses.post AS post, targets.email AS email, targets.fname AS fname, targets.lname AS lname, campaigns_responses.ip AS ip, campaigns_responses.browser AS browser, campaigns_responses.browser_version AS browser_version, campaigns_responses.os AS os, campaigns_responses.link_time AS link_time FROM campaigns_responses JOIN targets ON campaigns_responses.target_id=targets.id JOIN campaigns ON campaigns.id = campaigns_responses.campaign_id LEFT JOIN education ON campaigns.education_id = education.id JOIN templates ON templates.id = campaigns.template_id" ) or die ( '<div id="die_error">There is a problem with the database...please try again later</div>' );
+$r = mysql_query ( $sql ) or die ( '<div id="die_error">There is a problem with the database...please try again later</div>' );
 
+//set header
+$output = "Campaign,Template,Education,EducationTiming,Date,Year,Time,FirstName,LastName,TargetEmail,LinkTime,Post,IP,Browser,OS,Group";
+
+//get all the custom metrics from the targets table and into an array
+$metrics = mysql_query ( "SHOW COLUMNS FROM targets" );
+
+//add custom metrics to header
+while ( $metrics_results = mysql_fetch_array ( $metrics ) ) {
+    if ( $metrics_results['Field'] != "id" && $metrics_results['Field'] != "email" && $metrics_results['Field'] != "fname" && $metrics_results['Field'] != "lname" && $metrics_results['Field'] != "group_name" ) {
+        $output .= "," . $metrics_results['Field'];
+    }
+}
+$output .= "\n";
+
+//output to data
 while ( $ra = mysql_fetch_assoc ( $r ) ) {
-    $output .= $ra['campaign_name'] . "," . $ra['template_name'] . "," . $ra['education_name'] . "," . $ra['education_timing'] . "," . $ra['date_sent'] . "," . $ra['fname'] . "," . $ra['lname'] . "," . $ra['email'] . "," . $ra['link_time'] . "," . $ra['post'] . "," . $ra['ip'] . "," . $ra['browser'] . "," . $ra['os'] . "\n";
+    $output .= $ra['campaign_name'] . "," . $ra['template_name'] . "," . $ra['education_name'] . ",";
+    if($ra['education_timing'] == 0){
+        $output .= ",";
+    }
+    if($ra['education_timing'] == 1){
+        $output .= "Immediately,";
+    }
+    if($ra['education_timing'] == 2){
+        $output .= "After Form Submission,";
+    }
+    $output .= $ra['date_sent'] . "," . $ra['fname'] . "," . $ra['lname'] . "," . $ra['email'] . "," . $ra['link_time'] . "," . $ra['post'] . "," . $ra['ip'] . "," . $ra['browser'] . "," . $ra['os'] . "," . $ra['group_name'];
+    //get all the custom metrics from the targets table and into an array
+    $metrics = mysql_query ( "SHOW COLUMNS FROM targets" );
+    while ( $metrics_results = mysql_fetch_array ( $metrics ) ) {
+        if ( $metrics_results['Field'] != "id" && $metrics_results['Field'] != "email" && $metrics_results['Field'] != "fname" && $metrics_results['Field'] != "lname" && $metrics_results['Field'] != "group_name" ) {
+            $temp = $metrics_results['Field'];
+            $output .= "," . $ra[$temp];
+        }
+    }
+    $output .= "\n";
 }
 
 //setup file
