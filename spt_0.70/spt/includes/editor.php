544937433f58de259c3ed005885bda9678db975f
1 <1@sptoolkit.com>
2012-04-17 22:27:55 -0500

    Cleaned up TinyMCE a little

--- spt_0.70/spt/includes/editor.php
+++ spt_0.70/spt/includes/editor.php
@@ -2,7 +2,7 @@
 
 /**
  * file:    editor.php
- * version: 4.0
+ * version: 5.0
  * package: Simple Phishing Toolkit (spt)
  * component:	Core Files
  * copyright:	Copyright (C) 2011 The SPT Project. All rights reserved.
@@ -239,7 +239,7 @@ else {
     //start the textarea
     echo "
                 <form action=\"\" method=\"POST\">
-                    <textarea id=\"code\" name=\"code\" rows=\"30\">".$file."</textarea>
+                    <textarea id=\"code\" name=\"code\">".$file."</textarea>
                 </form>";
 }
 echo "
@@ -255,13 +255,13 @@ echo "
 		// General options
 		mode : \"textareas\",
 		theme : \"advanced\",
-		plugins : \"autolink,lists,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template,wordcount,advlist,autosave,fullpage\",
+		plugins : \"autolink,lists,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,xhtmlxtras,wordcount,advlist,fullpage\",
 
 		// Theme options
-		theme_advanced_buttons1 : \"bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,styleselect,formatselect,fontselect,fontsizeselect\",
-		theme_advanced_buttons2 : \"search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,help,code,|,insertdate,inserttime,preview,|,forecolor,backcolor\",
-		theme_advanced_buttons3 : \"tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,media,advhr,|,print,|,ltr,rtl,|,fullscreen\",
-		theme_advanced_buttons4 : \"insertlayer,moveforward,movebackward,absolute,|,styleprops,|,cite,abbr,acronym,del,ins,attribs,|,visualchars,nonbreaking,template,pagebreak,restoredraft\",
+		theme_advanced_buttons1 : \"preview,code,fullscreen,|,link,unlink,image,insertdate,inserttime,|,forecolor,backcolor,bullist,numlist,charmap,bold,italic,underline,strikethrough,justifyleft,justifycenter,justifyright,justifyfull,styleselect,formatselect,fontselect,fontsizeselect\",
+		theme_advanced_buttons2 : \"\",
+		theme_advanced_buttons3 : \"\",
+		theme_advanced_buttons4 : \"\",
 		theme_advanced_toolbar_location : \"top\",
 		theme_advanced_toolbar_align : \"left\",
 		theme_advanced_statusbar_location : \"bottom\",
