f73b1727280567223be05ce499081da15638f26f
Derek <derek@sptoolkit.com>
2012-12-23 23:39:58 -0600

    LDAP User Authentication Works!
    Now you can log in with an LDAP user.  Still need to allow for LDAP
    user that is a member of a group.

--- spt/login/validator.php
+++ spt/login/validator.php
@@ -2,7 +2,7 @@
 
 /**
  * file:    validator.php
- * version: 7.0
+ * version: 8.0
  * package: Simple Phishing Toolkit (spt)
  * component:	Login management
  * copyright:	Copyright (C) 2011 The SPT Project. All rights reserved.
@@ -27,70 +27,108 @@
 //upon successfull authentication, session variables are created that allow access
 //start the session
 session_start ();
-
 //pull the unique salt value
 include 'get_salt.php';
-
 //set an ip session variable with a salt to avoid session hijacking
 $_SESSION['ip'] = md5 ( $_SESSION['salt'] . $_SERVER['REMOTE_ADDR'] . $_SESSION['salt'] );
-
 //connect to the database
 include '../spt_config/mysql_config.php';
-
 //pull in username and password to temp variables
 $temp_u = $_POST['u'];
 $temp_p = $_POST['p'];
-
 //validate the username/email address
 if ( ! filter_var ( $temp_u, FILTER_VALIDATE_EMAIL ) ) {
     $_SESSION['alert_message'] = "invalid login attempt";
     header ( 'location:../' );
     exit;
 }
-
 //validate the username/email address length
 if ( strlen ( $temp_u ) > 50 ) {
     $_SESSION['alert_message'] = "invalid login attempt";
     header ( 'location:../' );
     exit;
 }
-
 //validate the password length
 if ( strlen ( $temp_p ) > 15 || strlen ( $temp_p ) < 8 ) {
     $_SESSION['alert_message'] = "invalid login attempt";
     header ( 'location:../' );
     exit;
 }
-
 //set variables to their final destination before the comparison occurs
 $u = $temp_u;
 $p = sha1 ( $_SESSION['salt'] . $temp_p . $_SESSION['salt'] );
-
 //grab all usernames and passwords from the database
 $r = mysql_query ( 'SELECT username, password, admin, disabled FROM users' ) or die ( '<div id="die_error">Error: Had trouble connection to database.</div>' );
-
 //start a loop to compare the data pulled from the database to the data submitted by user for each user in the database
 while ( $ra = mysql_fetch_assoc ( $r ) ) {
     //the actual comparison
     if ( $ra['username'] == $u && $ra['password'] == $p && $ra['disabled'] != 1 ) {
         //create an authenticated session
         $_SESSION['authenticated'] = 1;
-
         //create a username session
         $_SESSION['username'] = $u;
-
         //check to see if they are an admin
         if ( $ra['admin'] == 1 ) {
             //create an admin session
             $_SESSION['admin'] = 1;
         }
-
         //send authenticated user to the dashboard
         header ( 'location:../dashboard/#phish_pie' );
         exit;
     }
 }
-
+//grab all usernames from the ldap table
+$r = mysql_query ( 'SELECT username, admin, disabled, ldap_host FROM users_ldap' ) ;
+//start a loop to compare the data pulled from the database to the data submitted by user for each user in the database
+while ( $ra = mysql_fetch_assoc ( $r ) ) {
+    //the actual comparison
+    if ( $ra['username'] == $u && $ra['disabled'] != 1 ) {     
+        //get ldap host id
+        $ldap_server = $ra['ldap_host'];
+        //get ldap server details
+        $r1 = mysql_query("SELECT host, port, ssl_enc, ldaptype, bindaccount, aes_decrypt(password, '$spt_encrypt_key') as password, basedn FROM settings_ldap WHERE id = '$ldap_server'");
+        while($ra1 = mysql_fetch_assoc($r1)){
+            $ldap_host = $ra1['host'];
+            $ldap_port = $ra1['port'];
+            $ldap_ssl_enc = $ra1['ssl_enc'];
+            $ldap_ldaptype = $ra1['ldaptype'];
+            $ldap_bindaccount = $ra1['bindaccount'];
+            $ldap_password = $ra1['password'];
+            $ldap_basedn = $ra1['basedn'];
+        }
+        //attempt bind with provided username and password
+        //get ldap functions
+        include '../includes/ldap.php';
+        //get connected
+        $ldap_conn = ldap_connection($ldap_host,$ldap_port, $ldap_ssl_enc);
+        if(!$ldap_conn){
+            $_SESSION['alert_message'] = "problems attempting authentication";
+            header('location:../');
+            exit;
+        }
+        //get username
+        $ldap_user_lookup = ldap_user_email_query($ldap_host, $ldap_port, $ldap_bindaccount, $ldap_password, $ldap_basedn, $ldap_ssl_enc, $ldap_ldaptype, $temp_u);
+        if($ldap_user_lookup){
+            $ldap_dn = $ldap_user_lookup['0']['dn'];
+        }
+        //attempt bind with provided username and password
+        $ldap_bind = ldap_bind_connection($ldap_conn,$ldap_dn,$temp_p);
+        if($ldap_bind){
+            //create an authenticated session
+            $_SESSION['authenticated'] = 1;
+            //create a username session
+            $_SESSION['username'] = $u;
+            //check to see if they are an admin
+            if ( $ra['admin'] == 1 ) {
+                //create an admin session
+                $_SESSION['admin'] = 1;
+            }
+            //send authenticated user to the dashboard
+            header ( 'location:../dashboard/#phish_pie' );
+            exit;
+        }
+    }
+}
 //if they make it this far with no match then send them back to the login page with an error
 $_SESSION['alert_message'] = "invalid login attempt";
 header ( 'location:../' );
